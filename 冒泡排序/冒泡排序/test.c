b4d22eb6f4cde3cfd029e908d8dabfa2550ce7f2
yaobiao <3255483520@qq.com>
2021-06-01 11:43:34 +0800

    冒泡排序

---
+++ 冒泡排序/冒泡排序/test.c
@@ -0,0 +1,124 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//往往我们在写代码的时候，会将数组作为参数传个函数，比如：我要实现一个冒泡排序（这里要讲算法
+//思想）函数将一个整形数组排序。 那我们将会这样使用该函数：
+
+//void bubble_sort(int arr[])
+//{
+//	//确定冒泡排序的趟数
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		//每一趟冒泡排序
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j]>arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//
+//int main()//这个程序有点问题
+//{
+//	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//对arr进行排序，排成升序
+//	//arr是数组，我们对数组arr进行传参，实际上传递过去的是数组arr首元素的地址&arr[0]
+//	bubble_sort(arr);
+//	
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//	return 0;
+//}
+
+//void bubble_sort(int arr[],int sz)
+//{
+//	//确定冒泡排序的趟数
+//	int i = 0;
+//
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		//每一趟冒泡排序
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j]>arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	bubble_sort(arr,sz);
+//
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//	return 0;
+//}
+
+void bubble_sort(int arr[],int sz)
+{
+	//确定冒泡排序的趟数
+	int i = 0;
+
+	for (i = 0; i < sz - 1; i++)
+	{
+		int flag = 1;//假设这一趟要排序的数据已经有序
+		//每一趟冒泡排序	
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (arr[j]>arr[j + 1])
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+				flag = 0;//本趟排序的数据其实不完全有序
+			}
+		}
+		if (flag == 1)
+		{
+			break;
+		}
+	}
+}
+
+int main()
+{
+	int arr[] = {0,1,2,3,4,5,6,7,8,9 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+
+	bubble_sort(arr,sz);
+
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+}
