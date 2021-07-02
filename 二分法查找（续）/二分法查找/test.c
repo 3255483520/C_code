bc4e498f13e1442592f89e54fa47856c450c1081
yaobiao <3255483520@qq.com>
2021-07-02 10:14:20 +0800

    二分法查找（续）

---
+++ 二分法查找（续）/二分法查找/test.c
@@ -0,0 +1,167 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (arr[i] == k)
+//		{
+//			printf("找到了，下标是：%d\n", i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//	{
+//		printf("找不到\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (arr[i] == k)
+//		{
+//			printf("找到了，下标是：%d\n", i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//		printf("找不到\n");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int k = 17;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (arr[i] == k)
+//		{
+//			printf("找到了，下标是：%d\n", i);
+//			return 0;
+//		}
+//	}
+//	if (i == sz)
+//		printf("找不到\n");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (arr[i] == k)
+//		{
+//			printf("找到了，下标是：%d\n", i);
+//			return 0;
+//		}
+//	}
+//	printf("找不到\n");
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int k = 7;
+//
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//
+//		if (arr[mid]<k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid]>k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			printf("找到了，下标是：%d\n", mid);
+//			break;
+//		}
+//	}
+//	if (left > right)
+//	{
+//		printf("找不到指定的数字\n");
+//	}
+//
+//	return 0;
+//}
+
+
+int Binary_Search(int arr[], int k, int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+
+		if (arr[mid]<k)
+		{
+			left=mid+1;
+		}
+		else if (arr[mid]>k)
+		{
+			right=mid-1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	return -1;		
+}
+
+int main()
+{
+	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int k = 7;
+
+	int ret = Binary_Search(arr, k, sz);
+	if (ret == -1)
+	{
+		printf("找不到指定的数字\n");
+	}
+	else
+	{
+		printf("找到了，下标是：%d\n",ret);
+	}
+
+	return 0;
+}
