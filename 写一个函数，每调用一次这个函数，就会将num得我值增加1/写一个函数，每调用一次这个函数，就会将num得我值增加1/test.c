49f79827ac18167859f1daddec129a771023ffce
yaobiao <3255483520@qq.com>
2021-07-03 19:59:27 +0800

    写一个函数，每调用一次这个函数，就会将num得我值增加1（这个程序很好，有点绕，要弄清楚）

---
+++ 写一个函数，每调用一次这个函数，就会将num得我值增加1/写一个函数，每调用一次这个函数，就会将num得我值增加1/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//写一个函数，每调用一次这个函数，就会将num的值增加1
+void Add(int* p)
+{
+	(*p)++;
+}
+
+int main()
+{
+	int num = 0;
+	Add(&num);
+	printf("num=%d\n", num);//1
+	Add(&num);
+	printf("num=%d\n", num);//2
+	Add(&num);
+	printf("num=%d\n", num);//3
+
+	return 0;
+}
