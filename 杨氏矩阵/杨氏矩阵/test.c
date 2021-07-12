60076bac66489e64989cae1f655f899ce748a6b8
yaobiao <3255483520@qq.com>
2021-07-12 22:46:36 +0800

    杨氏矩阵（第三种方法最好）

---
+++ 杨氏矩阵/杨氏矩阵/test.c
@@ -0,0 +1,156 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//作业：
+//杨氏矩阵
+//有一个二维数组.
+//数组的每行从左到右是递增的，每列从上到下是递增的.
+//在这样的数组中查找一个数字是否存在。
+//时间复杂度小于O(N);
+//
+//数组：
+//1 2 3
+//2 3 4
+//3 4 5
+//
+//
+//1 3 4
+//2 4 5
+//4 5 6
+//
+//1 2 3
+//4 5 6
+//7 8 9
+
+//int FindNum(int arr[3][3], int k, int row, int col)
+//{
+//	int x = 0;//右上角元素的横坐标
+//	int y = col - 1;//右上角元素的纵坐标
+//
+//	while (x <= row - 1 && y >= 0)//x <= row - 1 && y >= 0这个条件要弄明白
+//	{
+//		if (arr[x][y] > k)	//右上角的元素>7,这一列都没有k	
+//		{
+//			y--;
+//		}
+//		else if (arr[x][y] < k)//右上角的元素<7,这一行都没有k	
+//		{
+//			x++;
+//		}
+//		else
+//		{
+//			return 1;
+//		}
+//	}
+//	//找不到
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+//	int k = 7;
+//	int ret = FindNum(arr, k, 3, 3);
+//	if (ret == 1)
+//	{
+//		printf("找到了\n");
+//	}
+//	else
+//	{
+//		printf("找不到\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//int FindNum(int arr[3][3], int k, int row, int col)
+//{
+//	int x = 0;
+//	int y = col - 1;
+//
+//	while (x <= row - 1 && y >= 0)
+//	{
+//		if (arr[x][y] > k)
+//		{
+//			y--;
+//		}
+//		else if (arr[x][y] < k)
+//		{
+//			x++;
+//		}
+//		else
+//		{
+//			printf("下标是%d %d\n", x, y);
+//			return 1;
+//		}
+//	}
+//	//找不到
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+//	int k = 7;
+//	int ret = FindNum(arr, k, 3, 3);
+//	if (ret == 1)
+//	{
+//		printf("找到了\n");
+//	}
+//	else
+//	{
+//		printf("找不到\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//这个程序思维上更好
+int FindNum(int arr[3][3], int k, int* px, int* py)
+{
+	int x = 0;
+	int y = *py - 1;
+
+	while (x <= *px - 1 && y >= 0)
+	{
+		if (arr[x][y] > k)
+		{
+			y--;
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			*px = x;
+			*py = y;
+			return 1;
+		}
+	}
+	//找不到
+	return 0;
+}
+
+int main()
+{
+	int arr[3][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+	int k = 7;
+	int x = 3;
+	int y = 3;
+	//返回型参数
+	int ret = FindNum(arr, k, &x, &y);
+	if (ret == 1)
+	{
+		printf("找到了\n");
+		printf("下标是%d %d\n", x, y);
+	}
+	else
+	{
+		printf("找不到\n");
+	}
+
+	return 0;
+}
